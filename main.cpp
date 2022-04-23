9fc83f04812888d5468c6bfc92a8c5f048dde211
E.M.Mondragon <xakarii@yahoo.com>
2022-04-23 14:01:17 -0500

    changed puzzle to node, added return value to findposmoves()

--- main.cpp
+++ main.cpp
@@ -1,49 +1,50 @@
 #include <iostream>
-#include "Puzzle.h"
-#include "Puzzle.cpp"
+#include "Node.h"
+#include "Node.cpp"
 #include <list>
 #include <vector>
 
-int heuristic1(Puzzle &p, Puzzle &g);
-int heuristic2(Puzzle &p, Puzzle &g);
-int heuristicS(Puzzle &p, Puzzle &g);
+int heuristic1(Node &p, Node &g);
+int heuristic2(Node &p, Node &g);
+int heuristicS(Node &p, Node &g);
+void aStarSearch(std::vector<Node> &puzzle, std::vector<Node> &goal, std::vector<Node> &bestPath);
 
 int main(int argc, char** argv) {
 	
 	
-	Puzzle g = Puzzle(1,2,3,8,0,4,7,6,5); //goal state
-	Puzzle initialOne = Puzzle(2,8,3,1,6,4,0,7,5);
-	Puzzle initialTwo = Puzzle(2,1,6,4,0,8,7,5,3);
-	Puzzle p = Puzzle();
-	std::vector<Puzzle> open, closed;
-	p.printPuzzle();
+	Node g = Node(1,2,3,8,0,4,7,6,5); //goal state
+	Node initialOne = Node(2,8,3,1,6,4,0,7,5);
+	Node initialTwo = Node(2,1,6,4,0,8,7,5,3);
+	Node p = Node();
+	std::vector<Node> open, closed, bestPath;
+	p.printNode();
 	std::cout<< std::endl << "Possible moves: ";
 	p.findPossibleMoves();
 
-	std::cout << "State at 2: "<<  p.getPuzzleState(2) << std::endl;
+	std::cout << "State at 2: "<<  p.getNodeState(2) << std::endl;
 	std::cout << "p.square[4]" << p.square[4] << std::endl;
 	std::cout << "Empty square is at : " << p.findEmpty() << std::endl;
 	//std::cout << "Swapping: 1 and empty 0: " << std::endl;
 	//p.swapPositions(1);
 	std::cout << "Empty square is at : " << p.findEmpty() << std::endl;
-	p.printPuzzle();
+	p.printNode();
 	std::cout << "p.square[1]" << p.square[1] << std::endl;
-	auto q = Puzzle(p);
-	q.printPuzzle();
+	auto q = Node(p);
+	q.printNode();
 	std::cout<< std::endl << "Possible moves: ";
 	q.findPossibleMoves();
 	
-	Puzzle r = Puzzle(1, 2, 3, 4, 5, 6, 7, 8, 0);
-	r.printPuzzle();
+	Node r = Node(1, 2, 3, 4, 5, 6, 7, 8, 0);
+	r.printNode();
 	std::cout << "Empty square is at : " << r.findEmpty() << std::endl;
 	std::cout<< std::endl << "Possible moves: ";
 	r.findPossibleMoves();
 	r.swapPositions(8);
-	r.printPuzzle();
+	r.printNode();
 	std::cout<<"intial state 1: " << std::endl;
-	initialOne.printPuzzle();
+	initialOne.printNode();
 	//std::cout<<"intial state 2: " << std::endl;
-	//initialTwo.printPuzzle();
+	//initialTwo.printNode();
 	
 	std::cout<<"possible moves for initial state 1" << std::endl;
 	initialOne.findPossibleMoves();
@@ -65,6 +66,46 @@ int main(int argc, char** argv) {
 	
 	return 0;
 }
+
+int heuristic1(Node &p, Node &g) {
+	int hValue;
+	for (int i = 0; i < 9; i++) {
+		std::cout<< "p.square[i] " << p.square[i] << " g.square[i] " << g.square[i] <<  " hValue "<< hValue << std::endl;
+		if (p.square[i] != g.square[i] && p.square[i] != 0) {
+			hValue++;
+		}
+	}
+	return hValue;
+}
+
+int heuristic2(Node &p, Node &g) {
+	std::cout << "Placeholder for H2" << std::endl;
+	//int tp 
+	//loop until p = g
+	//find possible moves
+	//pick the best one (may have to check grandkids)
+	//p =  "best one"
+	//calculate h(n)
+	//depth ++
+	// tp = tp + h(n) +1(for the extra debth)
+	//end loop
+	//print table
+}
+
+int heuristicS(Node &p, Node &g) {
+	std::cout << "Placeholder for S" << std::endl;
+}
+
+void aStarSearch(std::vector<Node> &puzzle, std::vector<Node> &goal, std::vector<Node> &bestPath) {
+	std::cout << "placeholder for A* search algo" << std::endl; 
+	//find h and f value of p and expand possible moves, swap each possible 
+	//with empty (0) square and create a new node for each
+	puzzle[0].findPossibleMoves();
+	
+	
+}
+
+
 //std:: vector<int> heuristicHelper(Puzzle &p, Puzzle &g, int function) {
 
 //finds possible moves
@@ -87,31 +128,3 @@ int main(int argc, char** argv) {
 //return vector<int>
 
 //}
-int heuristic1(Puzzle &p, Puzzle &g) {
-	int hValue;
-	for (int i = 0; i < 9; i++) {
-		std::cout<< "p.square[i] " << p.square[i] << " g.square[i] " << g.square[i] <<  " hValue "<< hValue << std::endl;
-		if (p.square[i] != g.square[i] && p.square[i] != 0) {
-			hValue++;
-		}
-	}
-	return hValue;
-}
-
-int heuristic2(Puzzle &p, Puzzle &g) {
-	std::cout << "Placeholder for H2" << std::endl;
-	//int tp 
-	//loop until p = g
-	//heuristicHelper(Puzzle &p, Puzzle &g, int function)
-	//p =  "best one"
-	//calculate h(n)
-	//depth ++
-	// tp = tp + h(n) +1(for the extra debth)
-	//print puzzle
-	//when p=g end loop
-	//print table
-}
-
-int heuristicS(Puzzle &p, Puzzle &g) {
-	std::cout << "Placeholder for S" << std::endl;
-}
